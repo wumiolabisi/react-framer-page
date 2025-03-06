f895448126baa304e01303e78795bad580ada6df
Wumi Olabisi <wumi.olabisi@gmail.com>
2025-03-06 15:24:22 +0100

    Initiation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import  { motion } from 'framer-motion';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <motion.button 
+      whileHover={{scale:1.1}}
+      whileTap={{scale:0.9}}
+      className='save-button'
+      onClick={() => null}>
+        Vidéo 1
+        </motion.button>
     </div>
   );
 }
